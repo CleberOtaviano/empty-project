5878d578632e2f62d855e123c7c4b8638fc2fb7a
Cléber Otaviano <cleberotaviano.core@gmail.com>
2016-06-19 08:43:35 -0300

    Update server.js

--- server.js
+++ server.js
@@ -47,3 +47,6 @@ app.get('/', function (req, res) {
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
+
+const MANOW = 'CARALHO QUE FODA --------------------------------------------------------';
+console.log(MANOW);

18d46a33bbe0dfa9950bd2c088228e3be5e2f0f4
Cléber Otaviano <cleberotaviano.core@gmail.com>
2016-06-19 07:46:31 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,40 @@
 var express = require('express');
 var app = express();
 
+// Bring Mongoose into the app
+var mongoose = require( 'mongoose' );
+
+// Build the connection string
+var dbURI = 'mongodb://localhost:27017/estampeirodb';
+
+// Create the database connection
+mongoose.connect(dbURI);
+
+// CONNECTION EVENTS
+// When successfully connected
+mongoose.connection.on('connected', function () {
+  console.log('CONECTADO','Mongoose default connection open to ' + dbURI);
+});
+
+// If the connection throws an error
+mongoose.connection.on('error',function (err) {
+  console.log('DEU RUIM','Mongoose default connection error: ' + err);
+});
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {
+  console.log('DISCONECTOU','Mongoose default connection disconnected');
+});
+
+// If the Node process ends, close the Mongoose connection
+process.on('SIGINT', function() {
+  mongoose.connection.close(function () {
+    console.log('Mongoose default connection disconnected through app termination');
+    process.exit(0);
+  });
+});
+
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
